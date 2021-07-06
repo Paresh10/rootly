673ebdf8dd1ee05df78baa72d050f8a47822b5ca
Paresh10 <paresh.sharma10@gmail.com>
2021-07-06 18:46:05 -0500

    Skipping Authenticity tokens

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,4 +1,5 @@
 class IncidentsController < ApplicationController
+	skip_before_action :verify_authenticity_token
 
 	def index
 		@incidents = Incident.all
@@ -12,6 +13,9 @@ class IncidentsController < ApplicationController
 		puts params
 		render "new"
 	end
+
+
+# I, [2021-07-06T23:39:59.777239 #67]  INFO -- : [0a0f49ee-d40f-462d-bbc6-2ec1f94ea4ed]   Parameters: {"token"=>"[FILTERED]", "team_id"=>"T026RA9B7T4", "team_domain"=>"coding-kgn2878", "channel_id"=>"C026JHPRZV4", "channel_name"=>"random", "user_id"=>"U027407427K", "user_name"=>"paresh.sharma10", "command"=>"/rootly", "text"=>"", "api_app_id"=>"A026RAHCCRY", "is_enterprise_install"=>"false", "response_url"=>"https://hooks.slack.com/commands/T026RA9B7T4/2247361553331/7dNNVEKD0ZvgiZMSGt7HdQt5", "trigger_id"=>"2259814876305.2229349381922.dfe82b937e9bab84247fc2ab0e957049"}
 
 	def create
 		Incident.create!(
