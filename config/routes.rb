574fb7d29c69d927a5e4355a2988dfe0fbdd9d1a
Paresh10 <paresh.sharma10@gmail.com>
2021-07-05 19:10:04 -0500

    Added GET and Post request to match for Slack command

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
   Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'incidents#index'
-  resource :incidents, only: [:index, :new, :show, :create]
+  resource :incidents, only: [:index, :show, :create]
+  match '/incidents/new',    to: 'incidents#new', via: [:get, :post]
 end
