3d1d3615924b383024f1178a39fc089c94588bab
Paresh10 <paresh.sharma10@gmail.com>
2021-07-07 19:11:51 -0500

    Pushing

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -15,43 +15,52 @@ class IncidentsController < ApplicationController
 	end
 
 	def rootly_declare_title
-		@block =	{
-			  "trigger_id": params[:trigger_id],
-			  "dialog": {
-			    "title": "Request a Ride",
-			    "submit_label": "Request",
-			    "notify_on_cancel": true,
-			    "state": "Limo",
-			    "elements": [
-			        {
-			            "type": "text",
-			            "label": "Pickup Location",
-			            "name": "loc_origin"
-			        },
-			        {
-			            "type": "text",
-			            "label": "Dropoff Location",
-			            "name": "loc_destination"
-			        }
-			    ]
-			  }
-			}
 
+    trigger_id = params[:trigger_id]
 
-			uri = URI.parse("https://slack.com/api/dialog.open")
-			http = Net::HTTP.new(uri.host, uri.port)
-			http.use_ssl = true
-			http.post(uri, @block.to_json, {
-					"Content-Type" => "application/json",
-					"Accept" => "application/json",
-					"Authorization" => ENV['API_KEY']
-			})
+    dialog = {
+        "callback_id": "ryde-46e2b0",
+        "title": "Request a Ride",
+        "submit_label": "Request",
+        "notify_on_cancel": TRUE,
+        "state": "Limo",
+        "elements": [
+            {
+                "type": "text",
+                "label": "Pickup Location",
+                "name": "loc_origin"
+            },
+            {
+                "type": "text",
+                "label": "Dropoff Location",
+                "name": "loc_destination"
+            }
+        ]
+    }
 
-			respond_to do |format|
-				format.html
-			  format.js
-			end
+    api_data = {
+        "token": ENV['API_KEY'],
+        "trigger_id": trigger_id,
+        "dialog": dialog
+    }
+
+		uri = URI.parse("https://slack.com/api/dialog.open")
+		http = Net::HTTP.new(uri.host, uri.port)
+		http.use_ssl = true
+		res = http.post(uri, api_data.to_json, {
+				"Content-Type" => "application/json",
+				"Accept" => "application/json"
+		})
+
+		puts res.body
+
+		respond_to do |format|
+			format.html
+		  format.js
+		end
 	end
+
+
 
 
 # I, [2021-07-06T23:39:59.777239 #67]  INFO -- : [0a0f49ee-d40f-462d-bbc6-2ec1f94ea4ed]   Parameters: {"token"=>"[FILTERED]", "team_id"=>"T026RA9B7T4", "team_domain"=>"coding-kgn2878", "channel_id"=>"C026JHPRZV4", "channel_name"=>"random", "user_id"=>"U027407427K", "user_name"=>"paresh.sharma10", "command"=>"/rootly", "text"=>"", "api_app_id"=>"A026RAHCCRY", "is_enterprise_install"=>"false", "response_url"=>"https://hooks.slack.com/commands/T026RA9B7T4/2247361553331/7dNNVEKD0ZvgiZMSGt7HdQt5", "trigger_id"=>"2259814876305.2229349381922.dfe82b937e9bab84247fc2ab0e957049"}
