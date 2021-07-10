0d054ae9daff565da6ce5c072f9fcae91f24329c
Paresh10 <paresh.sharma10@gmail.com>
2021-07-10 16:09:25 -0500

    Handling user response

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -24,21 +24,40 @@ class IncidentsController < ApplicationController
 			"token": ENV['API_KEY'],
 		  "dialog": {
 		    "callback_id": "ryde-46e2b0",
-		    "title": "Request a Ride",
-		    "submit_label": "Request",
+		    "title": "Create a new Incident",
+		    "submit_label": "Create",
 		    "notify_on_cancel": true,
 		    "state": "Limo",
 		    "elements": [
 		        {
 		            "type": "text",
-		            "label": "Pickup Location",
-		            "name": "loc_origin"
+		            "label": "Title",
+		            "name": "title"
 		        },
 		        {
 		            "type": "text",
-		            "label": "Dropoff Location",
-		            "name": "loc_destination"
-		        }
+		            "label": "description",
+		            "name": "description"
+		        },
+						{
+						  "label": "Severity",
+						  "type": "select",
+						  "name": "severity",
+						  "options": [
+						    {
+						      "label": "Sev0",
+						      "value": "sev0"
+						    },
+						    {
+						      "label": "Sev1",
+						      "value": "sev1"
+						    },
+						    {
+						      "label": "Sev2",
+						      "value": "sev2"
+						    }
+						  ]
+						}
 		    ]
 		  }
 		}
@@ -65,12 +84,45 @@ class IncidentsController < ApplicationController
 
 def create_incident
 		puts params
-		binding.pry
+		Incident.create!(
+			title: params[:payload][:title],
+			description: params[:payload][:description],
+			severity: params[:payload][:severity],
+			created_at: current_user.time_zone
+		)
+
+		payload = 	{
+			  "trigger_id": trigger_id,
+				"token": ENV['API_KEY'],
+			  "dialog": {
+					"name": params[:payload][:title],
+					"is_private": false,
+					"team_id": params[:payload][:team].id,
+					"creator": params[:payload][:user].name
+				}
+			}
+
+
+		headers  = {
+			"Content-Type" => "application/json; charset=utf-8",
+			"Authorization" => "Bearer " + ENV['API_KEY']
+		}
+
+		uri = URI.parse("https://slack.com/api/conversations.create")
+		http = Net::HTTP.new(uri.host, uri.port)
+		http.use_ssl = true
+		res = http.post(uri, payload.to_json, headers)
+
+		puts "res.body"
+		puts res.body
+
+		redirect_to '/'
 
 end
 
-
-# I, [2021-07-06T23:39:59.777239 #67]  INFO -- : [0a0f49ee-d40f-462d-bbc6-2ec1f94ea4ed]   Parameters: {"token"=>"[FILTERED]", "team_id"=>"T026RA9B7T4", "team_domain"=>"coding-kgn2878", "channel_id"=>"C026JHPRZV4", "channel_name"=>"random", "user_id"=>"U027407427K", "user_name"=>"paresh.sharma10", "command"=>"/rootly", "text"=>"", "api_app_id"=>"A026RAHCCRY", "is_enterprise_install"=>"false", "response_url"=>"https://hooks.slack.com/commands/T026RA9B7T4/2247361553331/7dNNVEKD0ZvgiZMSGt7HdQt5", "trigger_id"=>"2259814876305.2229349381922.dfe82b937e9bab84247fc2ab0e957049"}
+#
+# INFO -- : [4f755a79-9ebc-4277-ada4-770053ae4f16]   Parameters: {"payload"=>"{\"type\":\"dialog_submission\",\"token\":\"0UcG8yTyUwEmgDvQBvevPQQ1\",\"action_ts\":\"1625949942.943998\",\"team\":{\"id\":\"T026RA9B7T4\",\"domain\":\"coding-kgn2878\"},\"user\":{\"id\":\"U027407427K\",\"name\":\"paresh.sharma10\"},\"channel\":{\"id\":\"C026RA9BR8S\",\"name\":\"general\"},\"is_enterprise_install\":false,\"enterprise\":null,\"submission\":{\"loc_origin\":\"Hey\",\"loc_destination\":\"There\"},\"callback_id\":\"ryde-46e2b0\",\"response_url\":\"https:\\/\\/hooks.slack.com\\/app\\/T026RA9B7T4\\/2284957568240\\/kjwkqlbcXTVMd3uHSXYiC3BS\",\"state\":\"Limo\"}"}
+# Jul 10 03:45:43 PM  {"payload"=>"{\"type\":\"dialog_submission\",\"token\":\"0UcG8yTyUwEmgDvQBvevPQQ1\",\"action_ts\":\"1625949942.943998\",\"team\":{\"id\":\"T026RA9B7T4\",\"domain\":\"coding-kgn2878\"},\"user\":{\"id\":\"U027407427K\",\"name\":\"paresh.sharma10\"},\"channel\":{\"id\":\"C026RA9BR8S\",\"name\":\"general\"},\"is_enterprise_install\":false,\"enterprise\":null,\"submission\":{\"loc_origin\":\"Hey\",\"loc_destination\":\"There\"},\"callback_id\":\"ryde-46e2b0\",\"response_url\":\"https:\\/\\/hooks.slack.com\\/app\\/T026RA9B7T4\\/2284957568240\\/kjwkqlbcXTVMd3uHSXYiC3BS\",\"state\":\"Limo\"}", "controller"=>"incidents", "action"=>"create_incident"}
 
 
 
@@ -80,12 +132,12 @@ end
 
 	def create
 		Incident.create!(
-			title: params[:incident][:title],
-			description: params[:incident][:description],
-			severity: params[:severity],
-			created_at: DateTime.now
+			title: params[:payload][:title],
+			description: params[:payload][:description],
+			severity: params[:payload][:severity],
+			created_at: current_user.time_zone
 		)
 
-		redirect_to '/'
+		# redirect_to '/'
 	end
 end
